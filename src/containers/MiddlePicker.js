1af0e9cab0d6ad5b0c65ac66758ac0ec2eb15001
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-03-20 12:39:00 +0100

    Arrow added to button

--- src/containers/MiddlePicker.js
+++ src/containers/MiddlePicker.js
@@ -18,8 +18,18 @@ export class MiddlePicker extends Component {
         return ( 
             <div className="middleContainer" style={{backgroundImage: `url(${background})`}}>
                 <div className="innerContainer">
-                    <Link to={this.props.linkStrategies} className="strategiButton"><h1 className="buttonText">Strategies</h1></Link>
-                    <Link to={this.props.linkSetups} className="setupButton"><h1 className="buttonText">Setups</h1></Link>
+                    <Link to={this.props.linkStrategies} className="strategiButton"><h1 className="buttonText">Strategies</h1>
+                    <div className="arrow">
+                        <div className="arrow-top"></div>
+                        <div className="arrow-bottom"></div>
+                    </div>
+                    </Link>
+                    <Link to={this.props.linkSetups} className="setupButton"><h1 className="buttonText">Setups</h1>
+                    <div className="arrow">
+                        <div className="arrow-top"></div>
+                        <div className="arrow-bottom"></div>
+                    </div>
+                    </Link>
                 </div>
             </div>
         );
