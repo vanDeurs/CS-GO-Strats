d5ddfe625e3e2bd63b6d95ea9a42a07d49b41db4
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-03-27 18:32:47 +0200

    stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use('/', express.static(`${__dirname}/client/build`));
 
