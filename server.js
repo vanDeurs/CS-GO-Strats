418ad18c20395d5aaa7507581c8782e4286bda26
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-03-27 17:18:01 +0200

    fix

--- server.js
+++ server.js
@@ -11,8 +11,4 @@ app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 
-app.get('/hello', (req, res, next) => {
-    res.send("Hello")
-})
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
