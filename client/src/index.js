3acf9b07e4d7233dc0e26b3c36b3d4ab304fdee3
Alexander Wilson van Deurs <alexwvd@outlook.com>
2018-03-27 18:19:22 +0200

    Changed to hashrouter

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './styles/index.css';
 import { RenderMap }            from './components/RenderMap';
 import { RenderStrategies }     from './components/RenderStrategies';
 import { RenderSetups }         from './components/RenderSetups';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, HashRouter, Link, Switch } from 'react-router-dom';
 import { RenderStrategySite } from './components/RenderStrategySite.js';
 
 export class App extends Component {
@@ -30,15 +30,15 @@ export class App extends Component {
       
     render(){
         return(
-            <BrowserRouter>
-                <div>
+            <HashRouter>
+                <Switch>
                     <Route exact path='/' component={Home}/>
                     <Route exact path='/:map' component={RenderMap}/>
                     <Route exact path='/:map/strategies' component={RenderStrategies}/>
                     <Route exact path='/:map/setups' component={RenderSetups}/>
                     {/* <Route exact path='/:map/:map/strategies/A' component={RenderStrategySite}/> */}
-                </div>
-            </BrowserRouter>
+                </Switch>
+            </HashRouter>
             // <p>{this.state.response}</p>
 
         )
